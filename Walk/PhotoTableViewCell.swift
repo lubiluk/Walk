ef2a3f36b16bae486d1ffa7c3f8208b36144187c
Paweł Gajewski <lubiluk@gmail.com>
2019-05-15 22:32:39 +0200

    user interface

---
+++ Walk/PhotoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTableViewCell.swift
+//  Walk
+//
+//  Created by Paweł Gajewski on 15/05/2019.
+//  Copyright © 2019 Paweł Gajewski. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
